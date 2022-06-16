a3822c24143c0f024ffb74b10aee003c968aed9b
Ryuvy <97616024+Ryuvy@users.noreply.github.com>
2022-06-16 11:03:16 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1655351671,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/frame-sheet0.png",
+		"images/car-sheet0.png",
+		"images/tree2-sheet0.png",
+		"images/star2-sheet0.png",
+		"images/bomb2-sheet0.png",
+		"images/gameover-sheet0.png",
+		"images/button-sheet0.png",
+		"images/bg1-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
